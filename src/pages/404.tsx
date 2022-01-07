4a1ca0c79e0668d0f2c0fdc131cf64aace1dabc7
JudeBoy <judegiordano@gmail.com>
2022-01-06 23:36:00 -0500

    Custom Error Page

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import { AppLink } from "@Elements";
+import React from "react";
+
+const NotFound: React.FC = (): JSX.Element => {
+	return (
+		<div className="m-auto text-center">
+			<div className="m-auto border-b-2 border-gray-200 max-w-[200px] pb-3">
+				<h1 className="font-medium text-8xl">
+					404
+				</h1>
+			</div>
+			<h3 className="pt-3 text-xl">the requested page does not exist</h3>
+			<h3 className="pt-3 text-xl">maybe try going <AppLink href="/">home</AppLink></h3>
+		</div>
+	);
+};
+
+export default NotFound;
